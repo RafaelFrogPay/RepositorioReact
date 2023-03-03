3bc5171001bbc39a86fcafc4f3c00a0a6d62e79a
Rafael4tunato <rafael.rosa@frogpay.com>
2023-03-03 14:49:31 -0300

    modified:   meu-projeto/src/App.js 	new file:   meu-projeto/src/components/Pessoa.js 	new file:   meu-projeto/src/components/SayMyName.js

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,11 +1,21 @@
 import './App.css'
 import HelloWorld from './components/HelloWorld'
-
+import SeyMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
 
 function App() {
+  const nome = 'Gabriel'
+
   return (
     <div className="App">
     <HelloWorld />
+    <SeyMyName nome='Rafael'/>
+    <SeyMyName nome={nome}/>
+    <Pessoa 
+    nome="Rafael" 
+    idade="25" 
+    profissao="Analista de suporte" 
+    foto="https://via.placeholder.com/150"/>
     </div>
   );
 }
---
+++ meu-projeto/src/components/SayMyName.js
@@ -0,0 +1,9 @@
+function SeyMyName (props){
+  return (
+    <div>
+      <p>Olá {props.nome}, tudo bem? </p>
+    </div>
+  )
+}
+
+export default SeyMyName
